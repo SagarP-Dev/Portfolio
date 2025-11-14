a679c855c0f1d3308477acb0286bdc83547b7a0f
Sagar Panigrahi <sagarpanigrahi807@gmail.com>
2025-11-14 19:18:40 +0530

    Add skills to the about page

--- app/(client)/about/page.tsx
+++ app/(client)/about/page.tsx
@@ -51,13 +51,20 @@ const BlogPage = () => {
           <h2 className="text-2xl font-bold mb-4">Skills</h2>
           <div className="flex flex-wrap gap-2">
             {[
+              {[
+              "Python",
+              "Django",
+              "Fast API",
               "JavaScript",
               "TypeScript",
+              "TailwindCSS",
               "React",
+              "Next.js",
+              "Framer Motion",
               "Node.js",
               "Express.js",
+              "SQL",
               "MongoDb",
-              "AWS",
               "Git",
             ].map((skill) => (
               <span
